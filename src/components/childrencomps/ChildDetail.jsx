8b60639d221e7da7c7cb0ef0d66de6324f123761
Rifqi Setiadi <setiadirifqi2638@gmail.com>
2022-12-03 09:46:32 +0700

    Change order layout when display >576px, display height and wheight information

--- src/components/childrencomps/ChildDetail.jsx
+++ src/components/childrencomps/ChildDetail.jsx
@@ -16,8 +16,11 @@ function ChildDetail({
   gender,
   birthPlace,
   birthDate,
+  height,
+  weight,
   momName,
   dadName,
+  posyanduId,
   posyanduName,
   bidanName,
   vaccination,
@@ -29,15 +32,18 @@ function ChildDetail({
       <div className="jumbotron jumbotron-fluid py-5 text-white">
         <div className="container mt-5">
           <h1 className="display-6">
-            Informasi Adek&nbsp;
+            Informasi Dek&nbsp;
             <span>{name}</span>
           </h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          <p className="lead">
+            Disini anda dapat melihat semua informasi ada tentang Dek&nbsp;
+            {name}
+          </p>
         </div>
       </div>
       <Container className="py-5 text-black">
         <Row className="justify-content-center">
-          <Col sm={12} md={6}>
+          <Col sm={12} md={6} className="order-sm-2 order-md-1">
             <h4 className="py-4">Informasi Balita</h4>
             <Table responsive="sm">
               <tbody>
@@ -52,6 +58,14 @@ function ChildDetail({
                 <tr>
                   <td>Tanggal Lahir: </td>
                   <td>{birthDate}</td>
+                </tr>
+                <tr>
+                  <td>Tinggi Badan: </td>
+                  <td>{height}</td>
+                </tr>
+                <tr>
+                  <td>Berat Badan: </td>
+                  <td>{weight}</td>
                 </tr>
               </tbody>
             </Table>
@@ -69,7 +83,15 @@ function ChildDetail({
                 </tr>
               </tbody>
             </Table>
-            <Button as={Link} to={`/parents-data/${parentId}`} variant="primary" className="my-2">Lihat Detail</Button>
+            <Button
+              as={Link}
+              to={`/parents-data/${parentId}`}
+              variant="primary"
+              arial-label="lihat detail informasi orang tua balita button"
+              className="my-2"
+            >
+              Lihat Detail
+            </Button>
 
             <h4 className="py-4">Informasi Imunisasi</h4>
             <Table responsive="sm">
@@ -78,6 +100,19 @@ function ChildDetail({
                   <td>Nama Posyandu: </td>
                   <td>{posyanduName}</td>
                 </tr>
+              </tbody>
+            </Table>
+            <Button
+              as={Link}
+              to={`/posyandu-data/${posyanduId}`}
+              variant="primary"
+              arial-label="lihat detail informasi orang tua balita button"
+              className="my-2"
+            >
+              Lihat Detail
+            </Button>
+            <Table responsive="sm">
+              <tbody>
                 <tr>
                   <td>Jenis Imunisasi: </td>
                   <td>{vaccination}</td>
@@ -93,8 +128,8 @@ function ChildDetail({
               </tbody>
             </Table>
           </Col>
-          <Col xs={12} sm={6} className="text-center py-4">
-            <FontAwesomeIcon icon={faUserCircle} className="child-detail faUserCircle" />
+          <Col xs={12} sm={6} className="text-center py-4 order-sm-1 order-md-2">
+            <FontAwesomeIcon icon={faUserCircle} className="fa-custom-style-2" />
           </Col>
         </Row>
       </Container>
