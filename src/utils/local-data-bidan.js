b1234670d02c2cbd2faa6be75645c3231e6d9434
Rifqi Setiadi <setiadirifqi2638@gmail.com>
2022-12-03 10:23:07 +0700

    add local-data for bidan

---
+++ src/utils/local-data-bidan.js
@@ -0,0 +1,44 @@
+/* eslint-disable prefer-const */
+let bidans = [
+  {
+    id: 'bidan-01',
+    bidanName: 'Simiati Rosidah',
+    bidanPhoneNumber: '087855667878',
+    address: 'Jl Letjen Haryono MT RT02/RW04 Ds.Majakerata Kec. Watukumpul, Pemalang,Jawa Tengah',
+  },
+  {
+    id: 'bidan-02',
+    bidanName: 'Rosmaidah Yusmiasih',
+    bidanPhoneNumber: '082399886754',
+    address: 'Jl Benda Jaya RT07/RW01 Ds.Majakerata Kec. Watukumpul, Pemalang,Jawa Tengah',
+  },
+  {
+    id: 'bidan-03',
+    bidanName: 'Wiwiek Sugiharti',
+    bidanPhoneNumber: '082655443214',
+    address: 'Jl Letjen Haryono MT RT02/RW04 Ds.Majakerata Kec. Watukumpul, Pemalang,Jawa Tengah',
+  },
+  {
+    id: 'bidan-04',
+    bidanName: 'Rihana Dewi',
+    bidanPhoneNumber: '087834569876',
+    address: 'Jl Letjen Parman RT06/RW04 Ds.Majakerata Kec. Watukumpul, Pemalang,Jawa Tengah',
+  },
+  {
+    id: 'bidan-05',
+    bidanName: 'Syahidah',
+    bidanPhoneNumber: '082157461999',
+    address: 'Jl Kp Sawah 44, RT04/RW03 Ds.Majakerata Kec. Watukumpul, Pemalang,Jawa Tengah',
+  },
+];
+
+function getAllBidan() {
+  return bidans;
+}
+
+function getBidan(id) {
+  const foundedBidan = bidans.find((bidan) => bidan.id === id);
+  return foundedBidan;
+}
+
+export { getAllBidan, getBidan };
