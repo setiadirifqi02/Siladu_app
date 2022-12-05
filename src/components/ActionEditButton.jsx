c72317a6d9267d6aa90940661bde40a92ad8943b
Rifqi Setiadi <setiadirifqi2638@gmail.com>
2022-12-06 06:08:12 +0700

    change props from onUpdate function props to link props

--- src/components/ActionEditButton.jsx
+++ src/components/ActionEditButton.jsx
@@ -1,18 +1,19 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { faPencil } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function ActionEditButton({ id, onUpdate }) {
+function ActionEditButton({ link }) {
   return (
     <div className="action-button">
       <button
+        type="button"
         className="action-button__edit-button"
         aria-label="edit-data-button"
-        onClick={() => onUpdate(id)}
       >
-        <FontAwesomeIcon icon={faPencil} />
+        <Link to={link}><FontAwesomeIcon icon={faPencil} /></Link>
       </button>
     </div>
   );
